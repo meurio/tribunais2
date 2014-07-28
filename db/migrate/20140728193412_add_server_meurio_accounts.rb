263e77a3e1a2e5f9140c12ab9a154345661f1cac
Nícolas Iensen <nicolas@iensen.me>
2014-07-28 20:56:31 -0300

    Add database port for Meu Rio Accounts server

--- db/migrate/20140728193412_add_server_meurio_accounts.rb
+++ db/migrate/20140728193412_add_server_meurio_accounts.rb
@@ -5,16 +5,33 @@ class AddServerMeurioAccounts < ActiveRecord::Migration
       raise "MEURIO_ACCOUNTS_DBHOST is missing" if ENV["MEURIO_ACCOUNTS_DBHOST"].nil?
       raise "MEURIO_ACCOUNTS_DBUSER is missing" if ENV["MEURIO_ACCOUNTS_DBUSER"].nil?
       raise "MEURIO_ACCOUNTS_DBPASS is missing" if ENV["MEURIO_ACCOUNTS_DBPASS"].nil?
+      raise "MEURIO_ACCOUNTS_DBPASS is missing" if ENV["MEURIO_ACCOUNTS_DBPORT"].nil?
       raise "DB_USERNAME is missing" if ENV["DB_USERNAME"].nil?
-      execute "CREATE EXTENSION postgres_fdw;"
-      execute "CREATE SERVER meurio_accounts FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}', host '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}');"
-      execute "CREATE USER MAPPING for #{ENV["DB_USERNAME"]} SERVER meurio_accounts OPTIONS (user '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}', password '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}');"
+
+      execute <<-SQL
+      CREATE EXTENSION postgres_fdw;
+
+      CREATE SERVER meurio_accounts
+      FOREIGN DATA WRAPPER postgres_fdw
+      OPTIONS (
+        dbname  '#{ENV["MEURIO_ACCOUNTS_DBNAME"]}',
+        host    '#{ENV["MEURIO_ACCOUNTS_DBHOST"]}',
+        port    '#{ENV["MEURIO_ACCOUNTS_DBPORT"]}',
+      );
+
+      CREATE USER MAPPING for #{ENV["DB_USERNAME"]}
+      SERVER meurio_accounts
+      OPTIONS (
+        user      '#{ENV["MEURIO_ACCOUNTS_DBUSER"]}',
+        password  '#{ENV["MEURIO_ACCOUNTS_DBPASS"]}'
+      );
+      SQL
     end
   end
 
   def down
     if Rails.env.production? || Rails.env.staging?
-        execute "DROP EXTENSION postgres_fdw CASCADE;"
+      execute "DROP EXTENSION postgres_fdw CASCADE;"
     end
   end
 end
