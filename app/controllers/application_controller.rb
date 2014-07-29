4c1697e767b3fc28b91ce9ebba929477fe2a0525
Josias Schneider <josias.ds@gmail.com>
2014-07-29 11:27:15 -0300

    [#75737242] add login_via_email filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :login_via_email
+
+  def login_via_email
+    if params[:login_email].present?
+      if @user = User.find_by email: params[:login_email]
+        redirect_to root_path
+      else
+        redirect_to root_path, notice: 'O usuário não foi encontrado'
+      end
+    end
+  end
 end
