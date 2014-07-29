fea757f1ea7cdfb6aafeec2286d084ab66a6f7f3
Nícolas Iensen <nicolas@iensen.me>
2014-07-29 14:41:29 -0300

    Add ip to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729152943) do
+ActiveRecord::Schema.define(version: 20140729173907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140729152943) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "ip"
   end
 
 end
