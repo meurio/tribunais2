08721f3589176ff5e867e79ce6e3682cb7daf65d
Nícolas Iensen <nicolas@iensen.me>
2014-07-31 15:25:54 -0300

    Add poke_gilmar_mendes mail

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,6 +2,10 @@ class AppMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def poke_gilmar_mendes tasks_user
-    mail(to: ENV['GILMAR_MENDES_EMAIL'], from: tasks_user.user.email, subject: 'Pressão!')
+    mail(
+      to: ENV['GILMAR_MENDES_EMAIL'],
+      from: tasks_user.user.email,
+      subject: 'Ministro Gilmar Mendes, vote a favor da ADPF 289'
+    )
   end
 end
