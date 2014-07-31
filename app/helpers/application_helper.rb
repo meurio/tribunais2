66e4f8c1d886358b2e179852b720618a3dbd875c
Nícolas Iensen <nicolas@iensen.me>
2014-07-31 15:25:18 -0300

    Add helper task_button_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def task_button_class task, accomplished_tasks
+    if accomplished_tasks.include?(task)
+      "accomplished icon-checkmark"
+    else
+      "icon-mail2"
+    end
+  end
 end
