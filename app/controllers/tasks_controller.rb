0c2c0621b0d97fa16381b6c1452ae4949b9250e5
Josias Schneider <josias.ds@gmail.com>
2014-08-05 17:23:32 -0300

    [#75738282] add unaccomplished_tasks to User

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.order(:position)
-    @current_task = @tasks.first
+    @tasks = Task.order(position: :asc)
     @task_accomplishment = TaskAccomplishment.new user: User.new
+
     @accomplished_tasks = current_user.present? ? current_user.tasks : []
+    @unaccomplished_tasks = current_user.present? ? current_user.unaccomplished_tasks : @tasks
+    
+    @current_task = @unaccomplished_tasks.first
   end
 end
