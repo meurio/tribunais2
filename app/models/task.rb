1f6a210ae188a908a33bd1cb962cd3e85abacd06
Nícolas Iensen <nicolas@iensen.me>
2014-08-06 14:54:00 -0300

    Update seed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   validates :mailchimp_list_uid, :position, :name, :mission, presence: true
-  
+  validates :slug, uniqueness: true
+
   has_many :task_accomplishments
   has_many :users, through: :task_accomplishment
   acts_as_list
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Task.create!(
+Task.create(
   name: "Enviar email para ministro do STF",
   mission: "Missão 1",
   mailchimp_list_uid: "1a",
   slug: "poke_gilmar_mendes",
-  position: 1
+  position: 1,
+  description: "O ministro do STF Gilmar Mendes, relator da ADPF 289, é o responsável pela análise mais detalhada do processo e pelo primeiro voto. Ou seja: é fundamental que ele saiba desde já que a sociedade quer o fim do julgamento de civis por tribunais militares."
 )
 
-Task.create!(
+Task.create(
   name: "Enviar email para Procurador Geral",
   mission: "Missão 2",
   mailchimp_list_uid: "1b",
   slug: "poke_rodrigo_janot",
-  position: 2
+  position: 2,
+  description: "Agora que você já pressionou o relator da ADPF 289, envie também uma mensagem para o Procurador Geral da República, Rodrigo Janot. É ele quem vai emitir a opinião do Ministério Público, e sua voz tem grande poder de influência sobre os ministros do STF. A pressão dos setores autoritários em cima do procurador é grande. Precisamos contra-atacar!"
 )
 
-Task.create!(
+Task.create(
   name: "Espalhar mobilização no Facebook",
   mission: "Missão 3",
   mailchimp_list_uid: "1c",
   slug: "share_on_facebook",
-  position: 3
+  position: 3,
+  description: "Temos que ter a participação de milhares de cidadãos para que a pressão popular seja mais forte que a influência dos setores autoritários. Compartilhe agora a página da mobilização e convoque seus amigos para a missão pela desmilitarização da justiça."
 )
