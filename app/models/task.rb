ffa493441acea52019edd15fe66e8025e0ad3b05
Josias Schneider <josias.ds@gmail.com>
2014-08-06 16:33:46 -0300

    [#75738282] validate uniqueness of Task's position

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,8 @@
 class Task < ActiveRecord::Base
   validates :mailchimp_list_uid, :position, :name, :mission, presence: true
+  validates :position, uniqueness: true
   
   has_many :task_accomplishments
   has_many :users, through: :task_accomplishment
   acts_as_list
 end
-
-# validate uniqueness of position
-# criar slug
-# after_create TaskAccomplishment -> if self.task.slug(:poke_gilmar_mendes)
-# partial para cada tarefa #id será o #slug
