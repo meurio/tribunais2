575dd8df7b1a4a8ba2e469d1bcc1ef6b2f6fb2d2
Nícolas Iensen <nicolas@iensen.me>
2014-08-06 15:44:19 -0300

    Add accomplished all state to the tasks panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,15 @@ module ApplicationHelper
   def task_button_class task, accomplished_tasks, current_task
     classes = []
 
-    if accomplished_tasks.include?(task)
-      classes << "accomplished" << "icon-checkmark"
-    end
-
     if task == current_task
       classes << "current" << "bubble"
     end
 
-    if task.slug == "poke_gilmar_mendes" || task.slug == "poke_rodrigo_janot"
+    if accomplished_tasks.include?(task)
+      classes << "accomplished" << "icon-checkmark"
+    elsif task.slug == "poke_gilmar_mendes" || task.slug == "poke_rodrigo_janot"
       classes << "icon-mail2"
-    end
-
-    if task.slug == "share_on_facebook"
+    elsif task.slug == "share_on_facebook"
       classes << "icon-facebook"
     end
 
