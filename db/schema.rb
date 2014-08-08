92dba03172b7ec33e4702f6468aedc755a0b1f58
Nícolas Iensen <nicolas@iensen.me>
2014-08-08 17:08:37 -0300

    Add email_signature_html to organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808145446) do
+ActiveRecord::Schema.define(version: 20140808200749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140808145446) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.text     "email_signature_html"
   end
 
   create_table "tasks", force: true do |t|
